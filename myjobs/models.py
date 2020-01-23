65417404561b2b069a785bcddc68fb05cdaff148
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-01-23 10:42:05 +0200

    add location as an attr

--- myjobs/models.py
+++ myjobs/models.py
@@ -5,5 +5,6 @@ class Job(models.Model):
     title = models.CharField(max_length=20)
     employer = models.CharField(max_length=30)
     description = models.TextField(max_length=300)
+    location = models.CharField(max_length=20, null=True)
     start_date = models.DateField(blank=False, null=False)
     end_date = models.DateField(blank=True, null=True)

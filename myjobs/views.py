8f5dc7b25e5ed1acc0e9185259764c30914f7e41
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-01-23 09:12:36 +0200

    Create view for homepage

--- myjobs/views.py
+++ myjobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Job
 # Create your views here.
+
+def home(request):
+    jobs = Job.objects.all()
+    return render(request, "home.html", {"jobs": jobs})

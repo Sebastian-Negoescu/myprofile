1611f5dc65fe894c856396e77d5832b09af61f66
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-01-23 09:06:07 +0200

    create class Job

--- myjobs/models.py
+++ myjobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=20)
+    employer = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    start_date = models.DateField(blank=False, null=False)
+    end_date = models.DateField(blank=True, null=True)
